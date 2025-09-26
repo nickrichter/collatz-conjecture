ccd2084c7b1c4f00b2f7cc91096663fc15348616
Nick Richter <nicholosrichter@gmail.com>
2025-09-26 13:41:35 -0500

    Polishing the JS
    Finished up JS functionality to handle
      - displaying/hiding error messages
      - format html output

--- _DEV/js/main.js
+++ _DEV/js/main.js
@@ -1,68 +1,47 @@
-let uniqueDigits, myInputNum;
-let myInput = document.getElementById('kapInput');
+let calculationResultsOutput, myInputNum;
+let myInput = document.getElementById('userInput');
 let calculationSteps = document.getElementById('calculation-steps--user-input');
-let calculationResultsOutput = '';
-// This is what the "magic number" is, after which the result will always return 6174.
-let kaprekarsConstant = 6174;
+let inputMaxDigits = 10;
+let conjectureEndpoint = 1;
 
 
 
 
-function checkSubjectUniqueDigits(theSubject) {
-	let subjectSet = new Set(theSubject.toString());
-	let setTestResult;
-	
-	calculationResultsOutput += '<div><p><strong>Input:</strong> <span class="numbers">' + theSubject +'</span></p>';
-	uniqueDigits = Array.from(subjectSet).join(',');
-	calculationResultsOutput += '<p><strong>Unique digits:</strong> <span class="numbers">' + uniqueDigits + '</span></p>';
-	
-	if (subjectSet.size > 1) {
-		setTestResult = 'pass';
-		return setTestResult;
-	}
-	else {
-		setTestResult = 'fail';
-		return setTestResult;
-	}
-}
-
-
-
-
-function kaprekarCalculation(myNumber) {
-	let theSubject = myNumber;
-	let subjectSmallArr = theSubject.toString().split('').sort();
-	let subjectLargeArr = Array.from(subjectSmallArr).reverse();
-	let subjectSmall = subjectSmallArr.join('');
-	let subjectLarge = subjectLargeArr.join('');
-	let newSubject = subjectLarge - subjectSmall;
-	
-	calculationResultsOutput += '<div class="arithmatic">';
-	calculationResultsOutput += subjectLarge + '<br>- ' + subjectSmall + '<hr>';
-	if(newSubject == kaprekarsConstant) { calculationResultsOutput += '<span class="solution">'; }
-	calculationResultsOutput += newSubject;
-	if(newSubject == kaprekarsConstant) { calculationResultsOutput += '</span>'; }
-	calculationResultsOutput += '</div></div>';
-	
-	if(newSubject < 1000) {
-		newSubject = ('0000' + newSubject).slice(-4);
+function isOdd(n) {
+	// If number is odd
+	if (n & 1 == 1) {
+		return true;
 	}
 	
-	return newSubject;
+	// If number is even
+	return false;
 }
 
 
 
 
 function crunchNumber(calculatedValue) {
-	while(calculatedValue != kaprekarsConstant) {
-		if (checkSubjectUniqueDigits(calculatedValue) == 'pass') {
-			calculatedValue = kaprekarCalculation(calculatedValue);
-		}
-		else {
-			break;
+	calculationResultsOutput += '<div>';
+	calculationResultsOutput += '<p><strong>Input:</strong> <span class="numbers">' + calculatedValue + '</span></p>';
+	
+	while(calculatedValue != conjectureEndpoint) {
+		if(isOdd(calculatedValue)) {
+			calculationResultsOutput += '<div class="arithmatic">(' + calculatedValue + ' &times; 3) &plus; 1 &equals; ';
+			calculatedValue = (calculatedValue * 3) + 1;
+			calculationResultsOutput += calculatedValue + '</div>';
+		}else{
+			calculationResultsOutput += '<div class="arithmatic">' + calculatedValue + ' &divide; 2 &equals; ';
+			calculatedValue = calculatedValue / 2;
+			
+			if(calculatedValue != conjectureEndpoint) {
+				calculationResultsOutput += calculatedValue + '</div>';
+			}else{
+				calculationResultsOutput += '<span class="solution">' + calculatedValue + '</span></div>';
+			}
 		}
 	}
+	
+	calculationResultsOutput += '</div>';
 	
 	calculationSteps.innerHTML = calculationResultsOutput;
 	setTimeout(function() {
@@ -80,26 +59,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	runCalculationButton.addEventListener('click', function() {
 		myInputNum = parseInt(myInput.value, 10);
 		
-		if(myInput.value.length == 4) {
-			if(myInputNum !== kaprekarsConstant) {
-				if(checkSubjectUniqueDigits(myInputNum) == 'pass') {
-					calculationResultsOutput = '';
-					crunchNumber(myInputNum);
-					uniqueDigits = '';
-					
-					document.getElementById('calculation-results').classList.remove('hide');
-					document.getElementById('error--unique-digits').classList.add('hide');
-					document.getElementById('error--kaprekars-constant').classList.add('hide');
-				}else{
-					document.getElementById('error--unique-digits').classList.remove('hide');
-					document.getElementById('error--kaprekars-constant').classList.add('hide');
-					document.getElementById('calculation-results').classList.add('hide');
-				}
-			}else{
-				document.getElementById('error--kaprekars-constant').classList.remove('hide');
-				document.getElementById('error--unique-digits').classList.add('hide');
-				document.getElementById('calculation-results').classList.add('hide');
-			}
+		if(myInput.value.length <= inputMaxDigits) {
+			calculationResultsOutput = '';
+			crunchNumber(myInputNum);
+			
+			document.getElementById('calculation-results').classList.remove('hide');
+			document.getElementById('error--digits-maximum').classList.add('hide');
+		}else{
+			document.getElementById('error--digits-maximum').classList.remove('hide');
+			document.getElementById('calculation-results').classList.add('hide');
 		}
 	}, false);
 });
